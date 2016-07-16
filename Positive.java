140b52ce791d3b352cf95f66956349c77b4ed678
Sandeep-gaur <rsandeep76@gmail.com>
2016-07-16 15:06:58 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package positive;
+
+import java.util.Scanner;
+
+
+public class Positive {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Enter the no:");
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a>0){
+            System.out.println("the no: is positive");
+        }
+            else if(a<0){
+                System.out.println("the no: is negative");
+            }
+            else{
+                System.out.println("the no:is zero");
+                }
+        }
+    }
+
